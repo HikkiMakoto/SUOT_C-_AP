f6f89ec3b647fcaf889d7e089407c116a156d64a
MAKOTO <56062906+HikkiMakoto@users.noreply.github.com>
2020-10-28 12:59:15 +0200

    M1

---
+++ Labs_M1/cpp_lab_1_2_6_1-A.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	vector <string> ip_table{ "127.0.0.1", "192.168.0.1", "192.168.1.100", "255.255.255.255" };
+
+	int x;
+
+	cin >> x;
+	if (x > 4) {
+		cout << "Too big - value must be smaller than or equal to vector size." << endl;
+	}
+	else {
+		vector <string> ip_output (ip_table);
+		ip_output.resize(x);
+		for (unsigned i = 0; i < ip_output.size(); i++) {
+			cout << ip_output[i] << " ";
+		} 
+		cout << endl;
+	}
+	return 0;
+}
